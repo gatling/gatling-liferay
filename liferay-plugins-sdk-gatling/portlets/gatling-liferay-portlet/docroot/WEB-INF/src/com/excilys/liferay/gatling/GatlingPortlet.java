8d1c03c37f4ce447f9338f59654685cde0247d43
benoitboyer <bboyer@excilys.com>
2014-07-08 11:05:56 +0200

    Ajout de la portlet de base

---
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
@@ -0,0 +1,11 @@
+package com.excilys.liferay.gatling;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class GatlingPortlet
+ */
+public class GatlingPortlet extends MVCPortlet {
+ 
+
+}
