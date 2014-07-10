7b4473d03c2a83cf2ae9dc9adcc07e9fe49ecfd5
benoitboyer <bboyer@excilys.com>
2014-07-10 12:31:34 +0200

    Ajout d'un scénario à tester

--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
@@ -1,11 +1,70 @@
 package com.excilys.liferay.gatling;
 
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.sample.model.Request;
+import com.liferay.sample.model.Scenario;
+import com.liferay.sample.model.Simulation;
+import com.liferay.sample.service.RequestLocalServiceUtil;
+import com.liferay.sample.service.ScenarioLocalServiceUtil;
+import com.liferay.sample.service.SimulationLocalServiceUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 
 /**
  * Portlet implementation class GatlingPortlet
  */
 public class GatlingPortlet extends MVCPortlet {
  
-
+	/**
+	 * Adds a new Simulation to the database.
+	 * 
+	 */
+	public void addSimulation(ActionRequest request, ActionResponse response)
+			throws Exception {
+		long primaryKey = CounterLocalServiceUtil.increment(Simulation.class.getName());	
+		Simulation simulation = SimulationLocalServiceUtil.createSimulation(primaryKey);
+		if(!ParamUtil.getString(request, "simulationName").isEmpty()) {
+			simulation.setName(ParamUtil.getString(request, "simulationName"));
+			SimulationLocalServiceUtil.addSimulation(simulation);
+		}
+		else {
+			SessionErrors.add(request, "simulation-name-error");
+			sendRedirect(request, response);
+		}
+	}	
+	
+	/**
+	 * Adds a new Scenario to the database.
+	 * 
+	 */
+	public void addScenario(ActionRequest request, ActionResponse response)
+			throws Exception {
+		Scenario scenario =ScenarioLocalServiceUtil.createScenario(0);
+		scenario.setScenario_id(ParamUtil.getLong(request, "scenarioId"));
+		scenario.setName(ParamUtil.getString(request, "scenarioName"));
+		scenario.setSimulation_id(ParamUtil.getLong(request, "simulationId"));
+		
+		ScenarioLocalServiceUtil.addScenario(scenario);
+		
+	}
+	
+	/**
+	 * Adds a new Request to the database.
+	 * 
+	 */
+	public void addRequest(ActionRequest request, ActionResponse response)
+			throws Exception {
+		Request requestScenario = RequestLocalServiceUtil.createRequest(0);
+		requestScenario.setRequest_id(ParamUtil.getLong(request, "requestId"));
+		requestScenario.setScenario_id(ParamUtil.getLong(request, "scenarioId"));
+		requestScenario.setUrl(ParamUtil.getString(request, "url"));
+		requestScenario.setRate(ParamUtil.getInteger(request, "rate"));
+		
+		RequestLocalServiceUtil.addRequest(requestScenario);
+	}
+	
 }
--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/html/gatling/view.jsp
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/html/gatling/view.jsp
@@ -1,15 +1,39 @@
-<%@include file="/html/gatling/header.jsp" %>
+<%@include file="/html/gatling/header.jsp"%>
 
-<%
-	String redirect = PortalUtil.getCurrentURL(renderRequest);	
-%>
+<div>
+	<h3>Liste des scénarios enregistrés</h3>
+	<c:choose>
+		<c:when test="${empty listScenario  }">
+			<p>Il n'y a pas de scénarios enregistrés !</p>
+		</c:when>
+		<c:otherwise>
+			<c:forEach items="${listScenario}" var="scenario">
+				<ol>
+					<li><c:out value="${scenario}"></c:out> <a href="#"><i
+							class="icon-wrench"></i></a></li>
+				</ol>
+			</c:forEach>
+		</c:otherwise>
+	</c:choose>
+</div>
+<hr>
+<aui:button id="newSimulation" value="Ajouter Simulation"></aui:button>
+<div id="divNewSimulation" hidden="true">
+	<h5>Nouvelle simulation</h5>
+	<portlet:actionURL name="addSimulation" var="addSimulationURL" windowState="normal" />
+	<aui:form post="${addSimulationURL }" name="simulation_fm" id="simulation_fm">
+		<aui:input label="nom-simulation" name="simulationName">
+			<aui:validator name="required"></aui:validator>
+		</aui:input>
+		<aui:button name="ajouter-simulation" type="submit"></aui:button>
+	</aui:form>
+</div>
 
-<aui:button-row>
-	<portlet:renderURL var="addSimulationURL">
-		<portlet:param name="mvcPath" value="/html/gatling/addSimulation.jsp" />
-		<portlet:param name="redirect" value="<%= redirect %>" />
-		<portlet:param name="p_p_isolated" value="1" />
-	</portlet:renderURL>
+<aui:script use="aui-base">
+	A.one("#newSimulation").on('click', function() {
+		A.one("#divNewSimulation").show();
+		this.set('disabled', true);
+		this.ancestor('.aui-button').addClass('aui-button-disabled');
+	});
 
-	<aui:button value="add-simulation" onClick="<%= addSimulationURL.toString() %>"/>
-</aui:button-row>
+</aui:script>
