0938bd62e64547077290852e8146822c83c200d0
sbenmoussa <sbenmoussa@excilys.com>
2014-07-11 11:47:31 +0200

    persistence requesr et scenario

--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
@@ -1,23 +1,27 @@
 package com.excilys.liferay.gatling;
 
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.sample.model.Request;
+import com.liferay.sample.model.Scenario;
+import com.liferay.sample.service.RequestLocalServiceUtil;
+import com.liferay.sample.service.ScenarioLocalServiceUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
-import java.util.Set;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
@@ -27,17 +31,10 @@ import javax.portlet.RenderResponse;
  */
 public class GatlingPortlet extends MVCPortlet {
 
-
-
-
-
 	@Override
 	public void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
-
-
 		
-		try {
-			
+		try {		
 			int sizeGroups  = GroupLocalServiceUtil.getGroupsCount();
 
 			List<Group> listGroups = GroupLocalServiceUtil.getGroups(0, sizeGroups);
@@ -45,13 +42,86 @@ public class GatlingPortlet extends MVCPortlet {
 			
 			List<Layout> listLayouts = LayoutLocalServiceUtil.getLayouts(groupId, false);
 			
-			renderRequest.setAttribute("setGroup", listGroups);
+			renderRequest.setAttribute("listGroup", listGroups);
 			renderRequest.setAttribute("listLayout", listLayouts);
+			
 		} catch (SystemException e) {
 			e.printStackTrace();
 		}finally {
 			super.doView(renderRequest, renderResponse);
 		}
 	}
+	
+	
+	/**
+	 * Adds a new Request to the database
+	 * 
+	 */
+	public void addRequest(ActionRequest request, ActionResponse response)
+		throws Exception {
+
+		_log.info("addRequest contrôleur");
+		
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)request.getAttribute(WebKeys.THEME_DISPLAY);
+
+		
+
+		//update request list of scenario
+		List<Request> listUrlToStress = (List<Request>) request.getAttribute("listUrlToStress");
+		int totalRate = ParamUtil.getInteger(request,"totalRate");
+		if(listUrlToStress == null){
+			listUrlToStress = new ArrayList<Request>();
+		}
+				
+		totalRate += ParamUtil.getInteger(request, "rate");
+		_log.info("totalRate= "+totalRate+ " rate= "+ParamUtil.getInteger(request, "rate"));
+		_log.info("url= "+ParamUtil.getString(request, "url"));
+		if(totalRate ==100){
+			_log.info("total rate = 100 --> create request ");
+			//create request
+			Request newRequest = RequestLocalServiceUtil.createRequest(0);
+			newRequest.setUrl(ParamUtil.getString(request, "url"));
+			newRequest.setRate(ParamUtil.getInteger(request, "rate"));
+			newRequest = RequestLocalServiceUtil.addRequest(newRequest);
+			_log.info("create request ok");
+			listUrlToStress.add(newRequest);
+		}
+		request.setAttribute("listUrlToStress", listUrlToStress);
+		request.setAttribute("totalRate", totalRate);
+		
+		sendRedirect(request, response);
+	}
+	
+	/**
+	 * Adds a new Scenario to the database
+	 * 
+	 */
+	public void addScenario(ActionRequest request, ActionResponse response)
+		throws Exception {
+
+		_log.info("addScenario contrôleur");
+		
+		//create scenario
+		Scenario scenario = ScenarioLocalServiceUtil.createScenario(0);
+
+		scenario.setName(ParamUtil.getString(request, "name"));
+		scenario.setSimulation_id(ParamUtil.getLong(request, "simulation_id"));
+		
+		scenario = ScenarioLocalServiceUtil.addScenario(scenario);
+
+		//update data in database 
+		List<Request> listUrlToStress = (List<Request>) request.getAttribute("listUrlToStress");
+		if(listUrlToStress != null){
+			for(Request req : listUrlToStress){
+				req.setScenario_id(scenario.getScenario_id());
+				RequestLocalServiceUtil.updateRequest(req);
+			}
+		}
+		
+		sendRedirect(request, response);
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(GatlingPortlet.class);
 
 }
--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/html/gatling/addScenarioTemp.jsp
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/html/gatling/addScenarioTemp.jsp
@@ -1,51 +1,59 @@
 <%@include file="/html/gatling/header.jsp" %>
 
 <%
+	String redirect = PortalUtil.getCurrentURL(renderRequest);
 	Request urlRequest = null;
 	Scenario scenario = null;
-	List<Request> listUrlToStress = new ArrayList<Request>();
+	int totalRate = ParamUtil.getInteger(request,"totalRate");
 %>
 
 <aui:model-context bean="<%= scenario %>" model="<%= Scenario.class %>" />
 <aui:model-context bean="<%= urlRequest %>" model="<%= Request.class %>" />
 
-<aui:form action="" method="POST" name="formulaireScenario">
+
+<portlet:renderURL var="addScenarioURL"/>
+<portlet:actionURL name="addRequest" var="addRequestURL" windowState="normal" />
+
+<aui:form action="<%= addScenarioURL %>" method="POST" name="formulaireScenario">
 	<aui:fieldset>
-		<aui:input type="hidden" name="total"  id="total" value="0" />	
+		<aui:input type="hidden" name="total"  id="total" value="<%= totalRate %>" />	
 		<table>
 		<tr> <aui:input type="text" name="nameScenario" /></tr>
 		<tr>
-			<td>
-				<aui:select name="url" id="url">
-					<aui:option selected=""> -- </aui:option>
-					<c:forEach var="layout" items='${ listLayout }'>
-					
-						<aui:option label="${layout.name}" value="${layout.friendlyURL}" />
-	
-					</c:forEach>
-				</aui:select>
-			</td>
-
 			
-			<td>			
-			<aui:input name="rate" id="rate" class="rate" >
-				<aui:validator name="required"/>
-				<aui:validator name="range"> [0,100]</aui:validator>
-				<aui:validator errorMessage="total-stess-rate-must-be-maximum-100% " name="custom"> 
-					function(val, fieldNode, ruleValue){
-					var total = document.getElementById("<portlet:namespace />total")
-						console.log("total= "+total.value);
-						return ( (total.value + val) == 100 ) ;
-					}
-				</aui:validator>
-			</aui:input> 
+			<aui:form action="" method="POST" name="formulaireRequest">
+				<td>
+					<aui:select name="url" id="url">
+						<aui:option selected=""> -- </aui:option>
+						<c:forEach var="layout" items='${ listLayout }'>
+						
+							<aui:option label="${layout.name}" value="${layout.friendlyURL}" />
+		
+						</c:forEach>
+					</aui:select>
+				</td>
 
-			</td>
-			<td>
-				<aui:button-row>
-					<aui:button type="button" value="Add Request" onClick="<%= renderResponse.getNamespace() + \"addRequest()\"%>"/>
-				</aui:button-row>
-			</td>
+				<td>			
+				<aui:input name="rate" id="rate" class="rate" >
+					<aui:validator name="required"/>
+					<aui:validator name="range"> [0,100]</aui:validator>
+					<aui:validator errorMessage="total-stess-rate-must-be-100% " name="custom"> 
+						function(val, fieldNode, ruleValue){
+						var total = document.getElementById("<portlet:namespace />total")
+							console.log("total= "+total.value);
+							return ( (total.value + val) == 100 ) ;
+						}
+					</aui:validator>
+				</aui:input> 
+	
+				</td>
+			
+				<td>
+					<aui:button-row>					
+						<aui:button type="button" value="Add Request" onClick="<%= addRequestURL%>"/>
+					</aui:button-row>
+				</td>
+			</aui:form>
 		</tr>
 		</table>
 	</aui:fieldset>
@@ -53,14 +61,20 @@
 	
 	<c:if test="${ listUrlToStress.size() != 0 }">
 		<table>
-			<c:forEach items="<%= listUrlToStress%>" var="urlToStress" varStatus="boucle">
+			<c:forEach items="${ listUrlToStress }" var="urlToStress" varStatus="boucle">
 				<tr> <td>${urlToStress.url} </td> <td>${urlToStress.rate} </td></tr>
 			</c:forEach>
 		</table>
 	</c:if>
 
 	<aui:button-row>
-		<aui:button type="submit" value="addScenario" onClick="<%= renderResponse.getNamespace() + \"addScenario()\"%>"/>
-		<aui:button type="cancel"  onClick="<%= renderResponse.getNamespace() + \"hideForm()\"%>" />
+		<aui:button type="submit" value="addScenario"/>
+		<portlet:renderURL var="homeURL">
+			<portlet:param name="mvcPath" value="/html/gatling/view.jsp" />
+			<portlet:param name="redirect" value="<%= redirect %>" />
+			<portlet:param name="" value=""/>
+			<portlet:param name="p_p_isolated" value="1" />
+		</portlet:renderURL>
+		<aui:button type="cancel"  onClick="<%= homeURL.toString() %>" />
 	</aui:button-row>
 </aui:form>
