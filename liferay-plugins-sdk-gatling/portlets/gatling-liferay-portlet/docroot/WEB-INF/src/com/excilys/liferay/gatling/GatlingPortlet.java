dbfc7e12080f99b933a0081440cf2eb299e76013
benoitboyer <bboyer@excilys.com>
2014-07-11 12:27:01 +0200

    Correction de la redirection de page

--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
@@ -31,7 +31,15 @@ public class GatlingPortlet extends MVCPortlet {
 
 
 	private static Log log = LogFactoryUtil.getLog(GatlingPortlet.class);
-
+	
+	protected String jspListSimulation, jspEditSimulation;
+	
+	@Override
+	public void init() throws PortletException {
+		jspListSimulation = getInitParameter("list-simulation-jsp");
+		jspEditSimulation = getInitParameter("edit-simulation-jsp");
+		super.init();
+	}
 
 	/**
 	 * Adds a new Simulation to the database.
@@ -84,13 +92,10 @@ public class GatlingPortlet extends MVCPortlet {
 	@Override
 	public void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
 		/* récupération de la value mvcPath */
-		/* Elle doit être déclarée dans portlet.xml pour pouvoir récupérer la page associée */
-		String page = ParamUtil.get(renderRequest, "mvcPath", "list-simulation-jsp"); 
-		/* on récupére la page associée au mvcPath */
-		String renderPagePath = getInitParameter(page);  
-		log.info(renderPagePath);
+		/* récupération du chemin de la prochaine jsp */
+		String page = ParamUtil.get(renderRequest, "page", jspListSimulation); 
 		/* liste des simulations */
-		if(page.isEmpty() || page.equals("list-simulation-jsp")) {
+		if(page.isEmpty() || page.equals(jspListSimulation)) {
 			List<Simulation> list = new ArrayList<>();
 			try {
 				list = SimulationLocalServiceUtil.getSimulations(0, SimulationLocalServiceUtil.getSimulationsCount());
@@ -99,12 +104,11 @@ public class GatlingPortlet extends MVCPortlet {
 			}
 			renderRequest.setAttribute("listSimulation", list);
 		}
-		else if(page.equals("edit-simulation-jsp")) {
+		else if(page.equals(jspEditSimulation)) {
 			log.info("hello from doview");
 		}
-
-		/* on redirige sur la page du mvcPath */
-		include(renderPagePath, renderRequest, renderResponse);
+		/* on redirige sur la jsp de page */
+		include(page, renderRequest, renderResponse);
 		/*
 		 * TODO : Remettre au bon endroit
 		try {
@@ -117,22 +121,22 @@ public class GatlingPortlet extends MVCPortlet {
 			List<Layout> listLayouts = LayoutLocalServiceUtil.getLayouts(groupId, false);
 			renderRequest.setAttribute("setGroup", listGroups);
 			renderRequest.setAttribute("listLayout", listLayouts);
-			
 
-			
+
+
 			List<Scenario> ls =new ArrayList<Scenario>();
 			try {
 				ls.addAll(ScenarioLocalServiceUtil.findBySimulationId(1));
 				l.info(ls.get(0).getName());
 				int sizeLs = ls.size();
 				l.info(ls.get(sizeLs-1).getName());
-				
+
 			} catch (SystemException e) {
 				e.printStackTrace();
 			}
 			renderRequest.setAttribute("ls", ls);
-			
-			
+
+
 		} catch (SystemException e) {
 			e.printStackTrace();
 		}finally {
--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/html/gatling/view.jsp
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/html/gatling/view.jsp
@@ -1,17 +1,17 @@
 <%@include file="/html/gatling/header.jsp"%>
 
 <div>
-	<h3>Liste des scénarios enregistrés (${listSimulation.size()})</h3>
+	<h3><liferay-ui:message key="simulation-list-header"/></h3>
 	<c:choose>
 		<c:when test="${empty listSimulation}">
-			<p>Il n'y a pas de simulations d'enregistrés !</p>
+			<p><liferay-ui:message key="simulation-list-empty"/></p>
 		</c:when>
 		<c:otherwise>
 			<c:forEach items="${listSimulation}" var="simulation">
 				<ul>
 					<li>
 						<portlet:renderURL var="editSimulationURL">
-							<portlet:param name="mvcPath" value="/html/gatling/editSimulation.jsp"/>
+							<portlet:param name="page" value="/html/gatling/editSimulation.jsp"/>
 							<portlet:param name="simulationId" value="${simulation.simulation_id }"/>						
 						</portlet:renderURL>
 						<c:out value="${simulation.name}"></c:out> 
@@ -24,17 +24,17 @@
 	</c:choose>
 </div>
 <hr>
-<aui:button id="newSimulation" value="Ajouter Simulation"></aui:button>
+<aui:button id="newSimulation" value="simulation-list-btn-add-simulation"></aui:button>
 
 <%--redirect to addSimulation --%>
 <portlet:actionURL name="addSimulation" var="addSimulationURL" windowState="normal"/>
 <%--Formulaire d'ajout --%>
 <div id="newFormSimulation" hidden="true">
 	<aui:form action="${addSimulationURL}" name="simulation_fm" id="simulation_fm" >
-		<aui:input label="nom-simulation" name="simulationName">
+		<aui:input label="simulation-list-form-nom-simulation" name="simulationName">
 			<aui:validator name="required"></aui:validator>
 		</aui:input>
-		<aui:button name="ajouter-simulation" type="submit"></aui:button>
+		<aui:button  type="submit"></aui:button>
 	</aui:form>
 </div>
 
@@ -46,7 +46,7 @@ YUI().use(
       {
         bodyContent: AUI().one("#newFormSimulation").html(),
         centered: true,
-        headerContent: '<h3>Créer nouvelle simulation</h3>',
+        headerContent: '<h3><liferay-ui:message key="simulation-list-form-header" /></h3>',
         modal: true,
         resizable: false,
         visible: false,
