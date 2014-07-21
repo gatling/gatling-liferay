6c5353c3e2f6539055fb6cd91af330caa801b6c3
sbenmoussa <sbenmoussa@excilys.com>
2014-07-21 12:21:25 +0200

    i18n help page

--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/src/com/excilys/liferay/gatling/GatlingPortlet.java
@@ -314,7 +314,7 @@ public class GatlingPortlet extends MVCPortlet {
 	}
 
 	/**
-	 *  	REmove scenario from database
+	 *  	Remove scenario from database
 	 * @param request
 	 * @param response
 	 * @throws Exception
