c5f1a2693273c968028be4b585def3fb8e910498
benoitboyer <bboyer@excilys.com>
2014-11-12 10:12:24 +0100

    CLEAN Rebuild service with CE (remove META-INF/ service.properties and services/)

--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/service/com/excilys/liferay/gatling/model/ScenarioWrapper.java
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/WEB-INF/service/com/excilys/liferay/gatling/model/ScenarioWrapper.java
@@ -355,7 +355,7 @@ public class ScenarioWrapper implements Scenario, ModelWrapper<Scenario> {
 	}
 
 	@Override
-	public int compareTo(com.excilys.liferay.gatling.model.Scenario scenario) {
+	public int compareTo(Scenario scenario) {
 		return _scenario.compareTo(scenario);
 	}
 
@@ -365,17 +365,17 @@ public class ScenarioWrapper implements Scenario, ModelWrapper<Scenario> {
 	}
 
 	@Override
-	public com.liferay.portal.model.CacheModel<com.excilys.liferay.gatling.model.Scenario> toCacheModel() {
+	public com.liferay.portal.model.CacheModel<Scenario> toCacheModel() {
 		return _scenario.toCacheModel();
 	}
 
 	@Override
-	public com.excilys.liferay.gatling.model.Scenario toEscapedModel() {
+	public Scenario toEscapedModel() {
 		return new ScenarioWrapper(_scenario.toEscapedModel());
 	}
 
 	@Override
-	public com.excilys.liferay.gatling.model.Scenario toUnescapedModel() {
+	public Scenario toUnescapedModel() {
 		return new ScenarioWrapper(_scenario.toUnescapedModel());
 	}
 
