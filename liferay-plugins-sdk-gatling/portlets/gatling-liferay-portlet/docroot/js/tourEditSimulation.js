55dd6397cdb2abde09841076367f1c01d3cd7ba0
Benoit Boyer <bboyer@excilys.com>
2015-04-20 16:05:14 +0200

    Update take a tour

--- liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/js/tourEditSimulation.js
+++ liferay-plugins-sdk-gatling/portlets/gatling-liferay-portlet/docroot/js/tourEditSimulation.js
@@ -28,35 +28,16 @@ tourEditSimu.addStep('list-scenario-new-scenario', {
 	attachTo: '#newScenario right'
 });
 
-if(document.getElementById("exportModalTemplate") != null) {
-	tourEditSimu.addStep('list-scenario-export', {
-		title:"<liferay-ui:message key='tour-list-scenario-export-title' />",
-		text: "<liferay-ui:message key='tour-list-scenario-export' />",
-		attachTo: '#exportToggle bottom',
-		advanceOn : "#exportToggle click",
-		buttons:[]
-	});
+tourEditSimu.addStep('list-scenario-export', {
+	title:"<liferay-ui:message key='tour-list-scenario-export-title' />",
+	text: "<liferay-ui:message key='tour-list-scenario-export' />",
+	attachTo: '#exportToggle bottom',
+	buttons:[{
+		text: "<liferay-ui:message key='tour-understood' />",
+		action: tourEditSimu.hide
+	}]
+});
 
-	tourEditSimu.addStep('list-scenario-export-gatling', {
-		title:"<liferay-ui:message key='tour-list-scenario-export-gatling-title' />",
-		text: "<liferay-ui:message key='tour-list-scenario-export-gatling' />",
-		attachTo: '#_gatling_WAR_gatlingliferayportlet_gatlingVersion right',
-		buttons:[{
-			text: "<liferay-ui:message key='tour-understood' />",
-			action: tourEditSimu.hide
-		}]
-	});
-} else {
-	tourEditSimu.addStep('list-scenario-export', {
-		title:"<liferay-ui:message key='tour-list-scenario-export-title' />",
-		text: "<liferay-ui:message key='tour-list-scenario-export' />",
-		attachTo: '#exportToggle bottom',
-		buttons:[{
-			text: "<liferay-ui:message key='tour-understood' />",
-			action: tourEditSimu.hide
-		}]
-	});
-}
 
 if(readCookie("tour") == "true") {
 	eraseCookie("tour");
