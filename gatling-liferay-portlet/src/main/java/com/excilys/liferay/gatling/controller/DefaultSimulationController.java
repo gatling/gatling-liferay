52d977c1e5810967a82f8c4682cd4ee41a0286d7
yann mougenel <ymougenel@excilys.com>
2016-07-26 17:44:26 +0200

    [Resources]: change the resources location and add a readFile method

--- gatling-liferay-portlet/src/main/java/com/excilys/liferay/gatling/controller/DefaultSimulationController.java
+++ gatling-liferay-portlet/src/main/java/com/excilys/liferay/gatling/controller/DefaultSimulationController.java
@@ -6,6 +6,10 @@ package com.excilys.liferay.gatling.controller;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
@@ -22,9 +26,37 @@ public class DefaultSimulationController {
 	@RenderMapping(params="render=seeOldPortlet")
 	public String seeOldStuff(final RenderRequest renderRequest, final RenderResponse renderResponse) {
 		LOG.debug("seeOldStuff called");
+		
 		return "oldView";
 	}
 
 
+	 // Get file loads a specific file from WEB-INF/classes 
+	 // https://web.liferay.com/community/forums/-/message_boards/message/10307074
+	 private String getFile(String path) {
+
+			StringBuilder result = new StringBuilder("");
+
+			//Get file from resources folder
+			ClassLoader classLoader = getClass().getClassLoader();
+			File file = new File(classLoader.getResource(path).getFile());
+			
+			//File f = new File( getClass().getClassLoader().getResource("/file.txt").getFile());
+			try (Scanner scanner = new Scanner(file)) {
+
+				while (scanner.hasNextLine()) {
+					String line = scanner.nextLine();
+					result.append(line).append("\n");
+				}
+
+				scanner.close();
+				System.out.println(result);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+				
+			return result.toString();
+
+		  }
 }
 
