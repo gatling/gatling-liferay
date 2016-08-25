0f83a8b8d475ed580985d1a9ba4e83f478e08692
yann mougenel <ymougenel@excilys.com>
2016-08-25 17:27:21 +0200

    [Clean uneeded file]

--- gatling-liferay-portlet/cleanDefaultModele.sql
+++
@@ -1,8 +0,0 @@
-delete from StressTool_UrlSiteMap;
-delete from StressTool_SiteMap;
-delete from StressTool_Process;
-delete from StressTool_Login;
-delete from StressTool_Scenario where name ="_default_scenario_";
-delete from StressTool_Simulation where name="_default_simulation_";
-
-
