d05758570f95203a7a948d68df5a9158e60d1b2b
nraymond <nraymond@excilys.com>
2016-08-26 11:46:05 +0200

    [RecordDataAST] Feature: Now generates get and post code

---
+++ gatling-liferay-portlet/src/main/java/com/excilys/liferay/gatling/model/AST/InvalidAST.java
@@ -0,0 +1,8 @@
+package com.excilys.liferay.gatling.model.AST;
+
+@SuppressWarnings("serial")
+public class InvalidAST extends RuntimeException {
+
+	public InvalidAST() {}
+	
+}
--- gatling-liferay-portlet/src/main/java/com/excilys/liferay/gatling/model/AST/feeder/data/RecordDataAST.java
+++ gatling-liferay-portlet/src/main/java/com/excilys/liferay/gatling/model/AST/feeder/data/RecordDataAST.java
@@ -1,12 +1,15 @@
 package com.excilys.liferay.gatling.model.AST.feeder.data;
 
 import com.excilys.liferay.gatling.model.UrlRecordType;
+import com.excilys.liferay.gatling.model.AST.InvalidAST;
 import com.excilys.liferay.gatling.model.AST.feeder.ResourceFileAST;
 
 public class RecordDataAST {
 
 	public static final String HEADER = "url,type,datafile";
 
+	public static final String BODIES = "bodies/";
+	
 	private String url;
 	private String type;
 	private ResourceFileAST data;
@@ -32,13 +35,43 @@ public class RecordDataAST {
 	public String getContent() {
 		StringBuilder contentBuilder = new StringBuilder("http(\"Page ")
 				.append(url)
-				.append("\")\n");
+				.append("\")\n      ");
+		
 		switch(UrlRecordType.valueOf(type)){
 			case GET:
+				contentBuilder.append(".get(\"")
+						.append(url)
+						.append("\")\n");
 				break;
+				
 			case MULTIPART:
+				contentBuilder.append(".post(\"")
+						.append(url)
+						.append("\")\n");
+				if(data != null){
+					String fileName = data.getName();
+					if(fileName.startsWith(BODIES)) {
+						fileName = fileName.substring(BODIES.length());
+					}
+					contentBuilder.append("      .bodyPart(RawFileBodyPart(\"")
+							.append(fileName)
+							.append("\"))\n");
+				}
+				else {
+					throw new InvalidAST();
+				}
 				break;
+				
 			case POST:
+				contentBuilder.append(".post(\"")
+				.append(url)
+				.append("\")\n");
+				if(data != null){
+					contentBuilder.append(data.getContent());
+				}
+				else {
+					throw new InvalidAST();
+				}
 				break;
 		}
 		return contentBuilder.toString();
